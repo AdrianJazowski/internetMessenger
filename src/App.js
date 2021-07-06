d409e504d181e17d6b478d34c54095d61fad8de6
Adrian Jazowski <adrian.jazowski@gmail.com>
2021-07-06 17:41:54 +0200

    created first structure in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 /** @format */
 
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/header/Header";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Router>
+        <>
+          <Switch>
+            <Route exact path="/">
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
